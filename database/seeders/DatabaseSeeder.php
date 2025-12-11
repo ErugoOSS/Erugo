d0ae1dc51ef23e8491c403e9a0c5b6a233117507
Dean Ward <dean@websetgo.net>
2025-12-11 20:36:21 +0000

    Update themes.sql to set default theme to inactive and modify DatabaseSeeder to activate 'Erugo 2026' if no theme is currently active

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Theme;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,5 +23,11 @@ class DatabaseSeeder extends Seeder
         $sql = file_get_contents(base_path('themes.sql'));
         //run the query, ignore errors
         DB::unprepared($sql);
+
+        // Set default theme for fresh installs only
+        // If no theme is currently active, set Erugo 2026 as the default
+        if (!Theme::where('active', true)->exists()) {
+            Theme::where('name', 'Erugo 2026')->update(['active' => true]);
+        }
     }
 }
