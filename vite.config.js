59e88f70612c06d223deb24788f07ffcbac10b5f
Dean Ward <dean@websetgo.net>
2025-12-17 13:56:47 +0000

    Tweaked development setup to suport remote docker hosts

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import autoprefixer from 'autoprefixer';
-
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/js/app.js"],
-            refresh: true,
-        }),
-        vue(),
-    ],
-    css: {
-        postcss: {
-            plugins: [autoprefixer],
-        },
-    },
-});
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    hmr: {
+      host: process.env.VITE_DEV_HOST || 'localhost'
+    }
+  },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true
+    }),
+    vue()
+  ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer]
+    }
+  }
+})
