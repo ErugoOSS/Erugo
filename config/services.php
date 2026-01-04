311e0aa56c853014ffbf49abd52925b3069661ab
Kosztyk <36381705+Kosztyk@users.noreply.github.com>
2026-01-04 18:22:31 +0200

    - Add Clamav capabilities and backend share function
    - Add Clamav capabilities and backend share function

--- config/services.php
+++ config/services.php
@@ -14,6 +14,13 @@ return [
     |
     */
 
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
@@ -33,6 +40,12 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+    'convertx' => [
+        'token' => env('CONVERTX_INTEGRATION_TOKEN'),
+        'user_id' => env('CONVERTX_INTEGRATION_USER_ID'),
+        'default_expiry_hours' => env('CONVERTX_DEFAULT_EXPIRY_HOURS', 168),
     ],
 
 ];
